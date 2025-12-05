b24ea69981493f17aaea0d53faf520b3a12e1693
Weerasak Chongnguluam <singpor@gmail.com>
2025-12-05 14:25:22 +0700

    Day 5

---
+++ day5-part1.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Range [2]int
+
+func main() {
+	var freshRanges []Range
+
+	scaner := bufio.NewScanner(os.Stdin)
+	for scaner.Scan() {
+		line := scaner.Text()
+		if line == "" {
+			break
+		}
+
+		var r Range
+		fmt.Sscanf(line, "%d-%d", &r[0], &r[1])
+		freshRanges = append(freshRanges, r)
+	}
+
+	var countFresh int
+	for scaner.Scan() {
+		line := scaner.Text()
+		var id int
+		fmt.Sscanf(line, "%d", &id)
+		for _, r := range freshRanges {
+			if id >= r[0] && id <= r[1] {
+				countFresh++
+				break
+			}
+		}
+	}
+
+	fmt.Println(countFresh)
+}
---
+++ day5-part2.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type Range [2]int
+
+func main() {
+	var freshRanges []Range
+
+	scaner := bufio.NewScanner(os.Stdin)
+	for scaner.Scan() {
+		line := scaner.Text()
+		if line == "" {
+			break
+		}
+
+		var r Range
+		fmt.Sscanf(line, "%d-%d", &r[0], &r[1])
+		freshRanges = append(freshRanges, r)
+	}
+
+	var countFresh int
+
+	var mergedFreshRanges []Range
+	for i := 0; i < len(freshRanges); i++ {
+		merged := false
+		for j := i + 1; j < len(freshRanges); j++ {
+			if freshRanges[j][0] > freshRanges[i][1] {
+				continue
+			}
+			if freshRanges[j][1] < freshRanges[i][0] {
+				continue
+			}
+
+			newRange := Range{
+				min(freshRanges[i][0], freshRanges[j][0]),
+				max(freshRanges[i][1], freshRanges[j][1]),
+			}
+			freshRanges[j] = newRange
+			merged = true
+		}
+		if !merged {
+			mergedFreshRanges = append(mergedFreshRanges, freshRanges[i])
+		}
+	}
+
+	for _, r := range mergedFreshRanges {
+		countFresh += r[1] - r[0] + 1
+	}
+
+	fmt.Println(countFresh)
+
+}
